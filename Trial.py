f7af2536c35b9b4ccbfd49c39ed87fecee961284
SRahul <rrahul849@gmail.com>
2020-11-22 18:24:01 +0530

    Stratified KFold

--- Trial.py
+++ Trial.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
+from sklearn.model_selection import StratifiedKFold
 
 
 def WineQualityWhite():
@@ -33,9 +34,18 @@ def WineQualityRed():
     # print(df.describe())
 
     y = df.pop("quality")
-    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=.25)
+    #X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=.25)
 
-    clf = SVC(class_weight={0:0.15, 1:0.85})
+    skf = StratifiedKFold(n_splits=2)
+    StratifiedKFold(n_splits=2, shuffle=True)
+    for train_index, test_index in skf.split(df, y):
+        X_train, X_test = df.iloc[train_index].values, df.iloc[test_index].values
+        y_train, y_test = y[train_index].values, y[test_index].values
+
+    print(y_train.mean(), y_test.mean())
+
+    # clf = SVC(class_weight={0:0.15, 1:0.85})
+    clf = SVC()
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print(classification_report(y_test, y_pred))
